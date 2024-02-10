b7718b7b5998695acc350fd1714e97eda71e8e13
riAnriAn0 <156357069+riAnriAn0@users.noreply.github.com>
2024-02-10 20:33:18 -0300

    ..

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+let areaNota = document.querySelector('#atraNota')
+let editar = document.querySelector('#aditar')
+let marcar = document.querySelector('#marca')
+let apagar = document.querySelector('#apagar')
+let novaNota = document.querySelector('#novaNota')
+let main = document.querySelector('#main')
+
+novaNota.addEventListener('click',function(){
+    main.innerHTML += (('<div id="cxNota"><div id="nota"> <p id="areaNota" > </p></div><div id="editarNota"><i  id="editar" class="fa-solid fa-pen"></i><i id="marcar"  class="fa-solid fa-check"></i> <i  id="apagar" class="fa-solid fa-trash"></i></div></div>'))
+})
